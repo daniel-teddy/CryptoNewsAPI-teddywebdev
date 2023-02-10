6589c280a73fe56bc9f51bea91ca0d8159e76610
teddy-webdev <89258830+teddy-webdev@users.noreply.github.com>
2023-02-10 16:52:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+const PORT = process.env.PORT || 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const app = express()
+
+const newspapers = [
+    {
+        name: 'coinmarketcap',
+        address: 'https://coinmarketcap.com/community/articles/',
+        base: 'https://coinmarketcap.com'
+    },
+    {
+        name: 'coingecko',
+        address: 'https://www.coingecko.com/en/news',
+        base: ''
+    },
+    {
+        name: 'cryptonews',
+        address: 'https://cryptonews.com/news/',
+        base: 'https://cryptonews.com'
+    },
+]
+
+
+const articles = []
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("coin")' || 'a:contains("crypto")' || 'a:contains("web")' || 'a:contains("token")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+
+        })
+})
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Crypto News API')
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("coin")' || 'a:contains("crypto")' || 'a:contains("web")' || 'a:contains("token")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))

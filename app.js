b467983743fa43d108cc4d1697d9b43d211079e1
where.is.one <89258830+daniel-teddy@users.noreply.github.com>
2024-09-19 19:26:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const requestRoutes = require("./routes/requestRoutes");
 
 const app = express();
+app.use(cors()); // Allow all origins
 app.use(express.json());
 
 app.use("/requests", requestRoutes);
